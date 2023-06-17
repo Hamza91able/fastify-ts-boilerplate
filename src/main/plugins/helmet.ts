82c34342549a1ff99437a49ccf1395a80bd72bca
Hamza Khan <m.hamzakhan91able@gmail.com>
2023-06-17 19:08:36 +0500

    Fixed comment

--- src/main/plugins/helmet.ts
+++ src/main/plugins/helmet.ts
@@ -3,11 +3,10 @@
 import { FastifyInstance } from "fastify";
 import fp from "fastify-plugin";
 
-
 /**
- * This plugins adds some utilities to handle http errors
+ * This plugins adds important security headers for Fastify.
  *
- * @see https://github.com/fastify/fastify-sensible
+ * @see https://github.com/fastify/fastify-helmet
  */
 module.exports = fp(async function (fastify: FastifyInstance) {
   fastify.register(require("@fastify/helmet"), {
